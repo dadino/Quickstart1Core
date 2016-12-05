acaa301c81ca4ae244c4956959e05d99ae010e49
Dadino <dadino88>
2016-12-05 10:44:58 +0100

    - version 0.3.6 - added label to SpinnerLayout

--- core/src/main/java/com/dadino/quickstart/core/widgets/LoadingSpinner.java
+++ core/src/main/java/com/dadino/quickstart/core/widgets/LoadingSpinner.java
@@ -1,13 +1,17 @@
 package com.dadino.quickstart.core.widgets;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.database.DataSetObserver;
+import android.support.annotation.StringRes;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.widget.AdapterView;
 import android.widget.FrameLayout;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
+import android.widget.TextView;
 
 import com.dadino.quickstart.core.R;
 
@@ -17,7 +21,9 @@ public class LoadingSpinner<T extends SpinnerAdapter> extends FrameLayout {
 	protected T           mAdapter;
 	private   ProgressBar progress;
 	private   Spinner     spinner;
+	private   TextView    label;
 	private   boolean     mLoading;
+	private   String      mLabel;
 
 	public LoadingSpinner(Context context) {
 		super(context);
@@ -26,11 +32,13 @@ public class LoadingSpinner<T extends SpinnerAdapter> extends FrameLayout {
 
 	public LoadingSpinner(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		setLabelFromAttributeSet(context, attrs);
 		init();
 	}
 
 	public LoadingSpinner(Context context, AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
+		setLabelFromAttributeSet(context, attrs);
 		init();
 	}
 
@@ -38,8 +46,24 @@ public class LoadingSpinner<T extends SpinnerAdapter> extends FrameLayout {
 		inflate(getContext(), R.layout.view_loading_spinner, this);
 		progress = (ProgressBar) findViewById(R.id.loading_spinner_progress);
 		spinner = (Spinner) findViewById(R.id.loading_spinner_spinner);
+		label = (TextView) findViewById(R.id.loading_spinner_label);
+
+		if (TextUtils.isEmpty(mLabel)) {
+			label.setVisibility(GONE);
+		} else {
+			label.setVisibility(VISIBLE);
+			label.setText(mLabel);
+		}
 
 		initialize();
+	}
+
+	private void setLabelFromAttributeSet(Context context, AttributeSet attrs) {
+		if (attrs != null) {
+			TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.BoundedView);
+			mLabel = a.getString(R.styleable.LoadingSpinner_ls_label);
+			a.recycle();
+		}
 	}
 
 	protected void initialize() {}
@@ -49,6 +73,14 @@ public class LoadingSpinner<T extends SpinnerAdapter> extends FrameLayout {
 
 		progress.setVisibility(loading ? VISIBLE : INVISIBLE);
 		spinner.setVisibility(loading ? INVISIBLE : VISIBLE);
+	}
+
+	public void setLabel(@StringRes int stringId) {
+		label.setText(stringId);
+	}
+
+	public void setLabel(String string) {
+		label.setText(string);
 	}
 
 	public void setOnItemSelectedListener(AdapterView.OnItemSelectedListener listener) {
